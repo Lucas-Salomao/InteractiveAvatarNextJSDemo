59c9c04d39ad3d023289f0a0be23ffcc14e6a0e1
Lucas Tadeu Monteiro Guedes Fernandes Salomão <lucastadeusalomao@gmail.com>
2025-02-26 06:52:11 -0300

    first commit

---
+++ global.d.ts
@@ -0,0 +1,26 @@
+// globals.d.ts
+interface SpeechRecognitionEvent extends Event {
+  results: SpeechRecognitionResultList;
+}
+
+interface SpeechRecognition extends EventTarget {
+  start(): void;
+  stop(): void;
+  abort(): void;
+  lang: string;
+  continuous: boolean;
+  interimResults: boolean;
+  onresult: (event: SpeechRecognitionEvent) => void;
+  onerror: (event: Event) => void;
+  onend: () => void;
+}
+
+declare var SpeechRecognition: {
+  prototype: SpeechRecognition;
+  new (): SpeechRecognition;
+};
+
+declare var webkitSpeechRecognition: {
+  prototype: SpeechRecognition;
+  new (): SpeechRecognition;
+};
