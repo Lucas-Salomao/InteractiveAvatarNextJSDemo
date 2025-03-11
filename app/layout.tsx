71a8f9bdecb124f6d52d823584aaf63e419101ce
Lucas Salomao <lucastadeusalomao@gmail.com>
2025-03-10 22:06:06 -0300

    alteracao de logo e nome da aplicacao

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,11 @@ const fontMono = FontMono({
 
 export const metadata: Metadata = {
   title: {
-    default: "HeyGen Interactive Avatar SDK Demo",
-    template: `%s - HeyGen Interactive Avatar SDK Demo`,
+    default: "Tutor Virtual SENAI São Paulo",
+    template: `%s - Tutor Virtual SENAI São Paulo`,
   },
   icons: {
-    icon: "/heygen-logo.png",
+    icon: "/senai-logo.png",
   },
 };
 
